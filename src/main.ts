51728e71b6eb75b646143dc09afcf09c86e9b99a
Michael Linschulte <m.linschulte@andagon.com>
2021-03-10 08:42:29 +0100

    Adding application insights

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,37 @@
+import {
+  ApplicationInsights,
+  SeverityLevel,
+} from '@microsoft/applicationinsights-web';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString: '',
+    disableFetchTracking: false, // report fetch requests
+    disableExceptionTracking: false, // report exceptions
+    enableUnhandledPromiseRejectionTracking: true, // report unhandled promise rejections
+    enableAutoRouteTracking: true, // track navigation inside the SPA
+  },
+});
+
+Vue.config.errorHandler = (err, vm, info) => {
+  // Mimic the behaviour of the default error handler
+  console.error(`[Vue warn]: Error in ${info}: "${err.toString()}"`);
+  console.error(err);
+
+  // Report the vue error
+  appInsights.trackException({
+    exception: err,
+    severityLevel: SeverityLevel.Error,
+  });
+};
+
+appInsights.loadAppInsights();
+appInsights.context.application.ver = process.env.VUE_APP_VERSION;
+appInsights.trackPageView();
 
 Vue.config.productionTip = false;
 
